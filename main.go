9b3c0b464ac5634de9533a96876c80638d74eb53
lanphgphm <lanphuongpham02@gmail.com>
2025-03-27 17:27:49 +0000

    WIP - ast builder

--- main.go
+++ main.go
@@ -5,6 +5,30 @@ import (
 	"os"
 	
 	"BigCooker/syntax"
-	"BigCooker/semantics"
-	"BigCooker/codegen"
+	"BigCooker/syntax/parser"
+	// "BigCooker/syntax/ast"
+
+	// "BigCooker/semantics"
+	// "BigCooker/codegen"
 )
+
+func main() {
+	if (len(os.Args) < 2) {
+		fmt.Println("Usage: bigcooker <source-file-name>")
+		os.exit(1)
+	}
+	sourceFile := os.Args[1]
+
+	// 1. Syntax 
+	astRoot, err := parser.ParseFile(sourceFile)
+	if (err != nil) {
+		fmt.Printf("Error parsing file: %v\n", err)
+		os.exit(1)
+	}
+
+	// 2. Semantics 
+
+	// 3. Code generation 
+
+	fmt.Println("Compilation completed.")
+}
