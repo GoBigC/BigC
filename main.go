4f7bca3490046406375e9e484acef59c39b1b557
BroKisD <khoaphan2601@gmail.com>
2025-04-08 00:04:49 +0700

    update syntax error message

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Please supply source file")
-        os.Exit(1)
-    }
-    
-    filename := os.Args[1]
-    
-    err := syntax.ProcessFile(filename)
-    if err != nil {
-        fmt.Printf("Error processing file: %v\n", err)
-        os.Exit(1)
-    }
-    
-    fmt.Println("Successfully processed file and generated artifacts.")
+	if len(os.Args) < 2 {
+		fmt.Println("Please supply source file")
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+
+	err := syntax.ProcessFile(filename)
+	if err != nil {
+		fmt.Printf("Syntax error(s): %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully processed file and generated artifacts.")
 }
--- pkg/syntax/syntax.go
+++ pkg/syntax/syntax.go
@@ -9,45 +9,49 @@ import (
 )
 
 func ParseFile(filename string) (*ast.Program, error) {
-    return parser.ParseFile(filename)
+	return parser.ParseFile(filename)
 }
 
 func GetCST(filename string) string {
-    return getCST(filename)
+	return getCST(filename)
 }
 
 func ProcessFile(filename string) error {
-    outputDir := "artifact"
+	outputDir := "artifact"
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		return err // Uncomment this to properly handle directory creation errors
+	}
 
-    if err := os.MkdirAll(outputDir, 0755); err != nil {
-        return err
-    }
-    
-    cst := getCST(filename)
-    cstFormatted := FormatParseTree(cst)
-    if err := os.WriteFile(outputDir+"/cst.txt", []byte(cstFormatted), 0644); err != nil {
-        return err
-    }
-    
-    program, err := parser.ParseFile(filename)
-    if err != nil {
-        return err
-    }
-    
-    var astBuffer bytes.Buffer
-    astBuffer.WriteString("========== AST ==========\n")
-    PrintAST(program, "", &astBuffer)
-    if err := os.WriteFile(outputDir+"/ast.txt", astBuffer.Bytes(), 0644); err != nil {
-        return err
-    }
-    
-    if err := DrawASTTree(program, outputDir+"/ast_tree.txt"); err != nil {
-        return err
-    }
-    
-    if err := DrawCSTTree(filename, outputDir+"/cst_tree.txt"); err != nil {
-        return err
-    }
-    
-    return nil
+	cst := getCST(filename)
+	cstFormatted := FormatParseTree(cst)
+	if err := os.WriteFile(outputDir+"/cst.txt", []byte(cstFormatted), 0644); err != nil {
+		return err // Uncomment this to properly handle file write errors
+	}
+
+	program, err := parser.ParseFile(filename)
+	if err != nil {
+		// If there's a parsing error, write the error to a file and return
+		if err := os.WriteFile(outputDir+"/parse_error.txt", []byte(err.Error()), 0644); err != nil {
+			return err
+		}
+		return err // Return here to prevent using the nil program
+	}
+
+	// Only proceed with AST operations if parsing was successful
+	var astBuffer bytes.Buffer
+	astBuffer.WriteString("========== AST ==========\n")
+	PrintAST(program, "", &astBuffer)
+	if err := os.WriteFile(outputDir+"/ast.txt", astBuffer.Bytes(), 0644); err != nil {
+		return err // Uncomment this to properly handle file write errors
+	}
+
+	if err := DrawASTTree(program, outputDir+"/ast_tree.txt"); err != nil {
+		return err // Uncomment this to properly handle tree drawing errors
+	}
+
+	if err := DrawCSTTree(filename, outputDir+"/cst_tree.txt"); err != nil {
+		return err // Uncomment this to properly handle tree drawing errors
+	}
+
+	return nil
 }
