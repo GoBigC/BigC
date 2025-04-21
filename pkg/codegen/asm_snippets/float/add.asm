9a741065c3275025cba6002beb5c6f73905bbe41
Sid <quan.nguyen.210257@student.fulbright.edu.vn>
2025-04-21 23:12:25 +0700

    adjust asm snippets

--- pkg/codegen/asm_snippets/float/add.asm
+++ pkg/codegen/asm_snippets/float/add.asm
@@ -1,13 +1,13 @@
 .data
-a: .double 6.9  # we assume all floats used in BigC to be float64
-b: .double 4.2
+float.1: .double 6.9  # we assume all floats used in BigC to be float64
+float.2: .double 4.2
 
 .text
 main:
-    la t0, a     
+    la t0, float.1     
     fld ft0, 0(t0) # t0 = a
     
-    la t1, b     
+    la t1, float.2
     fld ft1, 0(t1) # t1 = b
     
     fadd.d ft1, ft1, ft0   # t1 = t1 + t0
--- pkg/codegen/asm_snippets/int/add_1_imm.asm
+++ pkg/codegen/asm_snippets/int/add_1_imm.asm
@@ -1,13 +1,10 @@
-.data
-x: .word 8000 # Store the larger value inside the word
 .text
 main:
 # If at 1 value is in immediate range [-2048, 2047]
-    la t0, x     
-    lw t0, 0(t0) # t0 = 200 
-    addi t1, t0, 10   # t1 = t0 + 10
+    li t0, 200     # Load immediate value 200 into t0
+    addi t0, t0, 10   # t1 = t0 + 10
 
 # Print result
     li a7, 1
-    mv a0, t1
+    mv a0, t0
     ecall
